0cfd344441bfd3578778dfde7cf64d09d2b4ea47
gauravMishra <gauravmishra99696@gmail.com>
2025-09-12 10:53:53 +0530

    a fix

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 app.secret_key = 'UUU-3-B2026_CARD_COUSTMER88654_AUTH'
 
 # Dummy user
-USER = { 'username': 'UUU3', 'password': 'UUU-3-B2026_CARD' }
+USER = { 'username': 'UUU3', 'password': 'UUU-3-B2026_CARD_COUSTMER88654_AUTH' }
 
 @app.route('/')
 def home():
